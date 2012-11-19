6f0696a9e40c9499184687de88be4903f03a90a9
Max Leske <maxleske@gmail.com>
2012-11-19 18:12:16 +0100

    working autotools for OS X

--- runner_test/runner_test/main.c
+++ runner_test/runner_test/main.c
@@ -8,9 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "run_command.h"
+#include "limbo.h"
 #include <unistd.h>
-#include <errno.h>
 
 int main() {
     char *argv[] = {"/bin/cat", "/etc/hosts", 0};
